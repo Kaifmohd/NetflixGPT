fbda837d97ab43277502cb67a59a2b00a92e81c1
kaifmohd75 <kaifmohd75@gmail.com>
2024-02-11 19:16:56 +0530

    Some changes related to video not playing in mobile device

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -17,8 +17,8 @@ const VideoBackground = ({movieId}) => {
     <div className='w-screen'>
         <iframe
         className="w-screen aspect-video"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0`} title="YouTube video player"  
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&showinfo=1&playsinline=1&autohide=1&fs=0&iv_load_policy=3&cc_load_policy=0`} title="YouTube video player"  
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowsInlineMediaPlayback></iframe>
     </div>
   )
 }
