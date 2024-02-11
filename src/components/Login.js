5026b7914092f9b2395bf34293ba5b1d7916a31d
kaifmohd75 <kaifmohd75@gmail.com>
2024-02-11 18:43:36 +0530

    minor fix for sign in/up page

--- src/components/Login.js
+++ src/components/Login.js
@@ -71,7 +71,7 @@ const Login = () => {
     <div>
         <Header/>
         <div className="absolute">
-        <img className="h-screen object-cover"
+        <img className="h-screen w-screen object-cover"
         alt ="logo" src={BG_URL}/>
         </div>
         <form onSubmit={(e)=> e.preventDefault()} className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
